1fc5a21eb2f4eed9efbe5cc4ba7ccda23a2647e8
Bachir <160189523+diallochir@users.noreply.github.com>
2024-06-10 15:39:17 -0400

    Add files via upload
    first commit

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+function sendMail(event) {
+    event.preventDefault();
+
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    var mailtoLink = `mailto:diallochir@gmail.com?subject=Contact%20Form%20Submission&body=Name:%20${encodeURIComponent(name)}%0AEmail:%20${encodeURIComponent(email)}%0AMessage:%20${encodeURIComponent(message)}`;
+
+    window.location.href = mailtoLink;
+}
