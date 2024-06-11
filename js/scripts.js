0da90c9b455fbbb501aa6e75fe6fb82e0e499cbb
Bachir <160189523+diallochir@users.noreply.github.com>
2024-06-11 10:37:51 -0400

    second commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,23 @@
-function sendMail(event) {
-    event.preventDefault();
+document.getElementById("sendEmailButton").addEventListener("click", function() {
+    const name = document.getElementById("idName").value;
+    const email = document.getElementById("idEmail").value;
+    const message = document.getElementById("idMessage").value;
+    
 
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var message = document.getElementById('message').value;
-
-    var mailtoLink = `mailto:diallochir@gmail.com?subject=Contact%20Form%20Submission&body=Name:%20${encodeURIComponent(name)}%0AEmail:%20${encodeURIComponent(email)}%0AMessage:%20${encodeURIComponent(message)}`;
-
-    window.location.href = mailtoLink;
-}
+    if (!name || !email || !message) {
+        alert("Please review the fields in the form.");
+        return;
+    }
+    
+    Email.send({
+        SecureToken: "8c3bd232-7a9a-4516-8651-9fb19f2127c0",
+        To: "diallochir@gmail.com",
+        From: "diallochir@gmail.com",
+        Subject: `New contact from ${name}`,
+        Body: `Name: ${name} <br> Email: ${email} <br> Message: ${message}`
+    }).then(
+        message => alert("Email sent successfully!")
+    ).catch(
+        error => alert("Failed to send email: " + error)
+    );
+});
